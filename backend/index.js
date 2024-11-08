ec9570ccf3d871c8c33ed048ea4f26aaadced561
krishnaj01 <krishna.jhanwar2005@gmail.com>
2024-11-08 16:50:25 +0530

    backend server setted up

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require('./db');
+const express = require('express');
+var cors = require('cors');
+connectToMongo();
+const app = express();
+app.use(cors());
+const port = 2000;
+app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/expenses', require('./routes/expense'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
