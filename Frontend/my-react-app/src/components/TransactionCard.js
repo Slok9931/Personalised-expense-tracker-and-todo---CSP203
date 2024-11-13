14f3bc7d61ded706cf79bbcb8545e3f26958d0a1
krishnaj01 <krishna.jhanwar2005@gmail.com>
2024-11-13 22:17:01 +0530

    completing the transaction card

--- Frontend/my-react-app/src/components/TransactionCard.js
+++ Frontend/my-react-app/src/components/TransactionCard.js
@@ -106,7 +106,141 @@ const TransactionCard = ({ expense }) => {
                 ) : null}
               </div>
 
-          
+              <div className="mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Amount"
+                  id="eamount"
+                  name="eamount" // Match name with state key
+                  value={editExpense.eamount}
+                  required
+                  onChange={onEditChange}
+                />
+              </div>
+
+              <div className="d-flex gap-3 justify-content-between items-center">
+                <div className="mb-3">
+                  <input
+                    type="radio"
+                    id="etype1"
+                    name="etype" // Match name with state key
+                    value="dr"
+                    checked={editExpense.etype === "dr"}
+                    required
+                    onChange={onEditChange}
+                  />
+                  <label htmlFor="etype1" className="px-2">
+                    Expense
+                  </label>
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="radio"
+                    id="etype2"
+                    name="etype" // Match name with state key
+                    value="cr"
+                    checked={editExpense.etype === "cr"}
+                    required
+                    onChange={onEditChange}
+                  />
+                  <label htmlFor="etype2" className="px-2">
+                    Income
+                  </label>
+                </div>
+
+                <div className="mb-3">
+                  <select
+                    id="ecategory"
+                    name="ecategory" // Match name with state key
+                    value={editExpense.ecategory}
+                    required
+                    onChange={onEditChange}
+                    className="form-control"
+                  >
+                    <option value="">Select a category</option>
+                    {editExpense.etype === "dr" &&
+                      expenseCategories.map((category) => (
+                        <option key={category} value={category}>
+                          {category}
+                        </option>
+                      ))}
+                    {editExpense.etype === "cr" &&
+                      incomeCategories.map((category) => (
+                        <option key={category} value={category}>
+                          {category}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Title"
+                  id="etitle"
+                  name="etitle" // Match name with state key
+                  value={editExpense.etitle}
+                  required
+                  onChange={onEditChange}
+                />
+              </div>
+
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  className="form-control"
+                  placeholder="Description"
+                  id="edescription"
+                  name="edescription" // Match name with state key
+                  value={editExpense.edescription}
+                  onChange={onEditChange}
+                />
+              </div>
+
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="submit" className="btn btn-primary">
+                  Update
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="expense-card"
+      onClick={() => {
+        handleEditClick(expense);
+      }}
+    >
+      <div
+        className="card-image"
+        style={{ backgroundImage: `url(/images/${image}.png` }}
+      >
+        <div className="overlay-text">{expense.category}</div>
+      </div>
+      <div className="card-body">
+        <div className="card-info">
+          <div className="card-title">{expense.title}</div>
+          <div className="card-amount">
+            <div className={`${amountClass}`}>${expense.amount}</div>
+          </div>
+        </div>
+        <div className="card-description">{expense.description}</div>
+      </div>
+    </div>
           
   </>
 );
