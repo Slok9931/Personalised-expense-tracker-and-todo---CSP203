f71f6a16a29049cfd8e06b1928f4a1e30d041bc1
snehanagmoti <snehan@iitbhilai.ac.in>
2024-11-13 23:34:03 +0530

    signup.js completed

--- Frontend/my-react-app/src/components/Signup.js
+++ Frontend/my-react-app/src/components/Signup.js
@@ -54,8 +54,115 @@ const Signup = () => {
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
-  
-  
+  return (
+    <div className="main">
+      <div className="signup1">
+        <img src="/logo512.png" alt="Header" height="250"></img>
+        <h2 className="welcome">Welcome to</h2>
+        <h2 className="welcome">iNotebook!</h2>
+        <div className="welcome">Your notes are secured in the cloud</div>
+      </div>
+      <div className="signup2">
+        <form onSubmit={handleSubmit}>
+          <div className="email">
+            <label htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              className="input"
+              id="name"
+              name="name"
+              aria-describedby="emailHelp"
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="email">
+            <label htmlFor="username">
+              Username
+            </label>
+            <input
+              type="text"
+              className="input"
+              id="username"
+              name="username"
+              aria-describedby="emailHelp"
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="email">
+            <label htmlFor="email">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="input"
+              id="email"
+              name="email"
+              aria-describedby="emailHelp"
+              onChange={onChange}
+              required
+            />
+            <div id="emailHelp">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="email">
+            <label htmlFor="password">
+              Password
+            </label>
+            <div className="password">
+            <input
+              type={view}
+              className="input"
+              id="password"
+              name="password"
+              minLength={3}
+              required
+              onChange={onChange}
+            />
+            <span
+              className="material-symbols-outlined password2"
+              onClick={show}
+            >
+              {icon}
+            </span>
+            </div>
+          </div>
+          <div className="email">
+            <label htmlFor="cpassword">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="input"
+              id="cpassword"
+              name="cpassword"
+              minLength={3}
+              required
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="btns">
+            <button type="submit" className="btn btn-primary mt-5">
+              Sign Up
+            </button>
+            <span
+              className="link"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
